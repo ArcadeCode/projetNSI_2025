49720880ac2660626fae4fd18653940c8424f109
Dervaux Esteban <e.dervauxx@gmail.com>
2025-05-20 14:23:35 +0200

    Remove .bat and .sh files from Git tracking

--- app/buildozer.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-docker run --rm -it ^
-  --volume "%cd%:/home/user/hostcwd" ^
-  --volume "%USERPROFILE%\Desktop\.buildozer:/home/user/.buildozer" ^
-  ghcr.io/kivy/buildozer:latest %*
--- app/buildozer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-docker run --rm -it \
-  --volume "$(pwd):/home/user/hostcwd" \
-  --volume "$HOME/Desktop/.buildozer:/home/user/.buildozer" \
-  ghcr.io/kivy/buildozer:latest "$@"
